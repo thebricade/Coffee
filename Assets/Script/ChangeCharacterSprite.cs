82bd13616a4a5c716d558ba2e50a18e92a4a470d
Brianna <bs3864@nyu.edu>
2018-12-15 08:55:09 -0700

    the start of the thing

---
+++ Assets/Script/ChangeCharacterSprite.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCharacterSprite : MonoBehaviour
+{
+
+	public Sprite frontSprite, backSprite;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//when you move forward change to seeing back of character
+		if (Input.GetKeyDown(KeyCode.W))
+		{
+			GetComponent<SpriteRenderer>().sprite = backSprite; 
+		}else if (Input.GetKeyDown(KeyCode.S))
+		{
+			//move backwards see front of character
+			GetComponent<SpriteRenderer>().sprite = frontSprite;
+		}
+	}
+}
