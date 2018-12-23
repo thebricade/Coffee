6bafcf3118f590071d009da11668d468c5226d5f
Brianna <bs3864@nyu.edu>
2018-12-23 00:50:24 -0700

    Created talking that works with Fungus and 3D environment

---
+++ Assets/Script/CollisionWithDialog.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Fungus;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+using Collision = UnityEngine.Collision;
+
+public class CollisionWithDialog : MonoBehaviour
+{
+
+	public bool hasTalked; 
+
+	public Flowchart flowchart; 
+
+	// Use this for initialization
+	void Start ()
+	{
+		Debug.Log("test load");
+		hasTalked = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Shop") && hasTalked == false)
+		{
+			Debug.Log("test");
+			GetComponent<FirstPersonController>().enabled = false;
+			
+		}
+		
+		
+	}
+
+	public void turnOnWalk()
+	{
+		GetComponent<FirstPersonController>().enabled = true;
+		hasTalked = true;
+	}
+}
